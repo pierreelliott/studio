400d437225e8b0d8bb99f0cfbe54b35deb0cb11d
Shimon Doodkin <helpmepro1@gmail.com>
2020-02-25 14:19:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import find_packages, setup
 
 setup(
     name='studio',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
         'keras',
-        'tensorflow==1.6',
+        'tensorflow==1.*',
         'pandas',
-        'numpy==1.15.4',
+        'numpy==1.*',
         'talos',
         'sklearn',
         'requests',
